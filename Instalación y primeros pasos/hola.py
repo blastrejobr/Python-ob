b1e329ffc823b4bb488f9ee01fb00bcd9073ea26
Blas Alberto Trejo Barrón <blas098@gmail.com>
2023-03-29 14:45:26 -0600

    Creo carpeta que contiene ejercicios

---
+++ Instalación y primeros pasos/hola.py
@@ -0,0 +1,5 @@
+print("hola")
+print("adios")
+
+x= "5"
+print("x vale " + x)
---
+++ Instalación y primeros pasos/objetos.py
@@ -0,0 +1,6 @@
+class miclase:
+    def suma(self, a, b):
+        return a + b
+
+datos = miclase()
+print(datos.suma(2,2))
